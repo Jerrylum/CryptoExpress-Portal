e65d57175222fe393f7d198cc0c3010d9e8311a1
Fakeye <crapper.612@gmail.com>
2024-03-20 14:08:37 +0800

    Work in progress

--- components/QRScanner.tsx
+++ components/QRScanner.tsx
@@ -1,23 +1,25 @@
 import { QrcodeErrorCallback, QrcodeSuccessCallback } from "html5-qrcode";
 import { Html5QrcodeScanner, Html5QrcodeScannerConfig } from "html5-qrcode/esm/html5-qrcode-scanner";
-import { useEffect } from "react";
+import React from "react";
+import styles from "./QRScanner.module.scss";
 
 export function QRScanner(props: {
+  className?: string;
   QRconfig: Html5QrcodeScannerConfig;
   verbose: boolean;
   qrCodeSuccessCallback: QrcodeSuccessCallback;
   qrCodeErrorCallback: QrcodeErrorCallback | undefined;
 }) {
-  const qrcodeRegionId = "ElementId";
-  let html5QrcodeScanner: Html5QrcodeScanner | undefined = undefined;
-  useEffect(() => {
-    if (html5QrcodeScanner) return;
-    // when component mounts
-    const verbose = props.verbose === true;
-    // Suceess callback is required.
-    html5QrcodeScanner = new Html5QrcodeScanner(qrcodeRegionId, props.QRconfig, verbose);
+  const qrcodeRegionId = "qr-code-scanner-region";
+
+  React.useEffect(() => {
+    const html5QrcodeScanner = new Html5QrcodeScanner(qrcodeRegionId, props.QRconfig, props.verbose === true);
     html5QrcodeScanner.render(props.qrCodeSuccessCallback, props.qrCodeErrorCallback);
+
+    return () => {
+      html5QrcodeScanner.clear();
+    };
   }, []);
 
-  return <div id={qrcodeRegionId} />;
+  return <div id={qrcodeRegionId} className={[styles.QRScanner, (props.className ?? "")].join(" ")}/>;
 }
