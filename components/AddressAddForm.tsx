2a005fd982fca8797be4a5cd3a88499e592cfd92
Jerrylum <serverofjerry@gmail.com>
2024-03-20 21:30:55 +0800

    Refactor using classnames

--- components/AddressAddForm.tsx
+++ components/AddressAddForm.tsx
@@ -4,6 +4,7 @@ import { TextInput, Button } from "flowbite-react";
 import React from "react";
 import * as InternalAddressCollection from "@/internal/InternalAddressCollection";
 import { Semaphore } from "./SemaphoreHook";
+import classNames from "classnames";
 
 export function AddressAddForm(props: { semaphore: Semaphore }) {
   const ref1 = React.useRef<HTMLFormElement>(null);
@@ -36,7 +37,7 @@ export function AddressAddForm(props: { semaphore: Semaphore }) {
       <form
         ref={ref1}
         action={onAddAddress}
-        className={"max-w-[400px] w-full flex flex-col gap-2" + (!isAddFormOpen ? " hidden" : "")}>
+        className={classNames("max-w-[400px]", "w-full", "flex", "flex-col", "gap-2", {"hidden": !isAddFormOpen})}>
         <TextInput name="recipient" type="text" placeholder="Recipient" required className="flex-1" />
         <TextInput name="line1" type="text" placeholder="Line 1" required className="flex-1" />
         <TextInput name="line2" type="text" placeholder="Line 2" required className="flex-1" />
@@ -55,7 +56,7 @@ export function AddressAddForm(props: { semaphore: Semaphore }) {
       <form
         ref={ref2}
         action={onInsertAddress}
-        className={"max-w-[400px] w-full flex flex-col gap-2" + (!isAddFormOpen ? "" : " hidden")}>
+        className={classNames("max-w-[400px]", "w-full", "flex", "flex-col", "gap-2", {"hidden": isAddFormOpen})}>
         <TextInput name="secret" type="text" placeholder="Secret" required className="flex-1" />
         <div className="flex-1 flex items-center">
           <Button color="gray" type="submit" className="w-20">
